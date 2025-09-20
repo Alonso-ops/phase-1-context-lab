a3643bb01069c92642cf9f56b50d6ed82d38c9f3
Fabian A Ortiz Chavez <alonsofabianortiz@icloud.com>
2025-09-19 18:14:47 -0600

    contextlab

--- index.js
+++ index.js
@@ -21,3 +21,70 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord(arr) {
+return {
+    firstName: arr[0],
+    familyName: arr[1],
+    title: arr[2],
+    payPerHour: arr[3],
+    timeInEvents: [],
+    timeOutEvents: []
+ }
+}
+
+function createEmployeeRecords(arrOfArrs) {
+    return arrOfArrs.map(createEmployeeRecord)
+}
+
+function createTimeInEvent(dateTimeString) {
+    const [date, hour] = dateTimeString.split(" ")
+    this.timeInEvents.push({
+        type: "TimeIn",
+        date: date,
+        hour: parseInt(hour, 10)
+    });
+    return this
+}
+
+function createTimeOutEvent(dateTimeString) {
+    const [date, hour] = dateTimeString.split(" ")
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        date: date,
+        hour: parseInt(hour, 10)
+    });
+    return this
+}
+
+function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find(e => e.date === date)
+    const timeOut = this.timeOutEvents.find(e => e.date === date)
+    return (timeOut.hour - timeIn.hour) / 100
+
+}
+
+function wagesEarnedOnDate(date) {
+    const boundHoursWorkedOnDate = hoursWorkedOnDate.bind(this)
+    const hours = boundHoursWorkedOnDate(date)
+    return hours * this.payPerHour
+}
+
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(e => e.firstName === firstName)
+}
+
+function calculatePayroll(employeeRecords) {
+  return employeeRecords.reduce((sum, emp) => sum + allWagesFor.call(emp), 0)
+}
+module.eports = {
+    createEmployeeRecord,
+    createEmployeeRecords,
+    createTimeInEvent,
+    createTimeOutEvent,
+    hoursWorkedOnDate,
+    wagesEarnedOnDate,
+    allWagesFor,
+    findEmployeeByFirstName,
+    calculatePayroll,
+}
